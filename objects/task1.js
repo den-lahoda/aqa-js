074b03f815514c3a08e8e4388129aaf1a5c62431
Denys Lahoda <dj.den.lagoda@gmail.com>
2025-04-24 15:22:11 +0300

    Objects

---
+++ objects/task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "Денис";
+student.lastName = "Лагода";
+student.age = 26;
+student.courses = ["Англійська Мова", "Математика", "Історія"];
+
+console.log(student);
